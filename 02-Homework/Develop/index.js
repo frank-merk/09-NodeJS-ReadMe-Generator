cd2f2449feb86ed676871b3daf8dbc4574d40e78
Frank Merchlewitz <jlimhb@gmail.com>
2021-02-17 18:13:30 -0600

    Added some questions and requirements

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,7 +1,45 @@
-// TODO: Include packages needed for this application
+Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("utils/generateMarkdown.js")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+Create an array of questions for user input
+const questions = [{
+          type: 'input',
+          name: 'project',
+          message: 'What is your project title?'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Briefly describe what your project is and what it does'
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'What installations are required for your project?'
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'How does one use your project?'
+        },
+        {
+          type: 'input',
+          name: 'contributors',
+          message: 'Please list other contributors to this project if there are any'
+        },
+        {
+          type: 'input',
+          name: 'tests',
+          message: 'How does one run tests on this project?'
+        },
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'What type of license does this project have?'
+          },
+      ]];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
